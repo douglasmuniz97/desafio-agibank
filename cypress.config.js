9b119e7d248543e622e0e8ad913956ce4ba38b21
Douglas Muniz <82132832+douglasmuniz97@users.noreply.github.com>
2024-04-22 15:33:17 -0300

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://blogdoagi.com.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
